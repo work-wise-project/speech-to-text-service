6f35d71ae1cd3b1890ad417502c72a3fbad4f42a
Michalikhol <michali.khol@gmail.com>
2025-04-20 21:35:59 +0300

    added credential check and change the ffmpeg to not needed to be installed

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { mkdir } from 'fs/promises';
 import path from 'path';
 import { startServer } from './src/server';
+import { checkAuthConnection } from './src/utils';
 
 const main = async () => {
     await mkdir(path.join(__dirname, '/public'), { recursive: true });
+    await checkAuthConnection();
 
     startServer();
 };
