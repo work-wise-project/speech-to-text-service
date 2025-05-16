9dfd1664e8e2d53b43f15830ea617f2e15508ccc
idoakstein1 <idoakstein1@gmail.com>
2025-05-16 20:42:25 +0300

    added fix to folder creation

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import { mkdir } from 'fs/promises';
 import path from 'path';
 import { startServer } from './src/server';
+import { getConfig } from './src/services';
 import { checkAuthConnection } from './src/utils';
 
 const main = async () => {
-    await mkdir(path.join(__dirname, 'public'), { recursive: true });
+    const { isProductionEnv } = getConfig();
+
+    const publicFolderPath = isProductionEnv
+        ? path.resolve(__dirname, '..', 'public')
+        : path.resolve(__dirname, 'public');
+    await mkdir(publicFolderPath, { recursive: true });
+
     await checkAuthConnection();
 
     startServer();
